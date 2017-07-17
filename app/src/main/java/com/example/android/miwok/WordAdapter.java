4737828b3adcacd1dd1c3933d4d717b2582fbbbf
Vicuko <victorc.fernandez@gmail.com>
2017-07-17 20:29:56 +0200

    Create WordAdapter class for processing Word object and introducing it into the layout

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -1,16 +1,49 @@
 package com.example.android.miwok;
 
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Created by Vicuko on 17/7/17.
  */
 
-public class WordAdapter extends ArrayAdapter<Word>{
+public class WordAdapter extends ArrayAdapter<Word> {
+
+    public WordAdapter(Activity context, ArrayList<Word> words) {
+        super(context, 0, words);
+    }
+    @NonNull
     @Override
-    public View getView(int position, View convertView, ViewGroup parent){
-        return super.getView(position, convertView, parent);
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        // Get the {@link Word} object located at this position in the list
+        Word word = getItem(position);
+
+        // Find the TextView in the list_item.xml layout with the ID miwok_text
+        TextView miWokText = (TextView) listItemView.findViewById(R.id.miwok_text);
+
+        // Get the word from the current Word object and set it on the TextView
+        miWokText.setText(word.getMiWokTranslation());
+
+        // Find the TextView in the list_item.xml layout with the ID default_text
+        TextView defaultText = (TextView) listItemView.findViewById(R.id.default_text);
+
+        // Get the word from the current Word object and set it on the TextView
+        defaultText.setText(word.getDefautltTranslation());
+
+        return listItemView;
     }
 }
