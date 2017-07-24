c18cf0025c463618afb6bf68a08a84d4e3617c0b
Vicuko <victorc.fernandez@gmail.com>
2017-07-24 15:10:06 +0200

    Set ImageViews in WordAdapter

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -44,6 +45,12 @@ public class WordAdapter extends ArrayAdapter<Word> {
 
         // Get the word from the current Word object and set it on the TextView
         defaultText.setText(word.getDefautltTranslation());
+
+        // Find the ImageView in the list_item.xml layout with the ID image
+        ImageView image = (ImageView) listItemView.findViewById(R.id.image);
+
+        // Get the image from the current Word object and set it on the ImageView
+        image.setImageResource(word.getImageResourceId());
 
         return listItemView;
     }
