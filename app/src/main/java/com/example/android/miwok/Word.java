312e92d2d29069f76d2de2c6a863b241f640161f
Vicuko <victorc.fernandez@gmail.com>
2017-07-24 15:35:37 +0200

    Create hasImage method in Word class to check if the Word object contains an image

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -12,7 +12,9 @@ public class Word {
 
     private String mDefaultTranslation;
 
-    private int mImageResourceId;
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+
+    private static final int NO_IMAGE_PROVIDED = -1;
 
     public Word(String defaultTranslation, String miWokTranslation) {
         mMiWokTranslation = miWokTranslation;
@@ -42,6 +44,14 @@ public class Word {
 
     public int getImageResourceId(){
         return mImageResourceId;
+    }
+
+    /**
+     * Returns whether or not there is an image for this word.
+     * @return
+     */
+    public boolean hasImage(){
+        return mImageResourceId != NO_IMAGE_PROVIDED;
     }
 
 }
